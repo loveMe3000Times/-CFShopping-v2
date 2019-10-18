bfeba5c26ce0ce982cda266ad6a1b3ab9607eb5d
渐行渐远 <1280359335@qq.com>
2019-10-18 19:58:23 +0800

    fate: 完成了详情页的主要样式，以及购物车的主要样式

--- src/js/config.js
+++ src/js/config.js
@@ -3,6 +3,13 @@ require.config({
     paths: {
         "jquery": "libs/jquery/jquery-3.4.1.min",
         "header": "js/modules/header",
-        "template": "libs/art-template/template-web"
+        "template": "libs/art-template/template-web",
+        "jqueryCookie": "libs/jquery-plugins/jquery.cookie"
+    },
+    // 垫片: 垫片项目的运行，需要在其他包的基础上
+    shim: {
+        'jqueryCookie': {
+            deps: ['jquery']
+        }
     }
 });
