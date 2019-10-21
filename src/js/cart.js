5cdddf86901bee7944ef1ad19117662e41b971d7
渐行渐远 <1280359335@qq.com>
2019-10-21 21:09:29 +0800

    fate: 完成详情页购物车添加功能，完成购物车数量改变功能

--- src/js/cart.js
+++ src/js/cart.js
@@ -1,5 +1,69 @@
 require(['./config'], (config) => {
-    require(['header'], (header) => {
-        
+    require(['header', 'template'], (header, template) => {
+        class Cart{
+            constructor(){
+                this.initAll();
+                this.setNum();
+                this.shopTotal();
+            }
+            
+            /** 依据localstorage数据渲染界面商品内容区
+             *******************************************/
+            initAll(){
+                var list = JSON.parse(localStorage.getItem("cart")); // 本地购物车数据单
+
+                var text = template("shopContent", {list});
+                $("#supplierContent").html(text);
+            }
+
+            /** 增减购物数量
+             ***********************************/
+            setNum() {
+                $(".numWarp .warp").on('click', 'div', function() {
+                    var $this = $(this); // 点击对象
+                    var $num = null; // 数量框
+                    
+                    if($this.hasClass("less")){ //减少
+                        $num = $this.next("input");
+                        var num = parseInt($num.val()) - 1;
+                        if(num < 1){num = 1}
+                        $num.val(num);
+                    }else if($this.hasClass("more")){  // 添加
+                        $num = $this.prev("input");
+                        var num = parseInt($num.val()) + 1;
+                        $num.val(num);
+                    }
+                });
+            }
+
+            /** 当数量发生改变时，改变该条商品的总价格
+             *********************************************/
+            shopTotal(){
+                $(".numWarp .warp").on('click', changeTotal);
+                $(".numWarp .warp input").on('change', changeTotal);
+
+                // 改变总价
+                function changeTotal() {
+                    var id = $(this).parents(".goods").attr("index"); // 产品id
+                    var num = $(this).find("input").val();     // 选购数量
+                    var $total = $(this).parents(".goods").find(".total");
+                    
+                    // 改变值
+                    var list = JSON.parse(localStorage.getItem("cart"));
+                    list.forEach((item, index) => {
+                        if(id === item.id){
+                            var total =  parseFloat((parseFloat(num) * parseFloat(item.price))).toFixed(2);
+                            item.num = num;
+                            item.total = total;
+                            $total.html(total);
+                            localStorage.setItem("cart", JSON.stringify(list));
+                            return true;
+                        }
+                    });
+                }
+            }
+        }
+
+        return new Cart();
     });
 });
--- src/js/detail.js
+++ src/js/detail.js
@@ -1,12 +1,24 @@
+/** 详情页逻辑代码
+ * 
+ * @function initAll()    初始化界面（包括：样式详情的渲染，放大镜的初始化）
+ * @function setNum()     设置选购的数量,由加减按钮，控制input框中选购数量的变化
+ * @function changeImg()   选取不同的展示图片(小)时，改变当前图片为正在展示状态（class="ac"）
+ * @function changeLike()  改变"枪模"（like）或"款式"的选中样式 (class="ac")
+ * @function addCart()     实现点击 "添加购物车" 按钮，实现添加购物车功能（将部分重要信息存储在本地LocalStorage）
+ *******************************************************************************/
+
 require(['./config'], (config) => {
     require(['header', 'imgZoom', 'template'], (header, imgZoom, template) => {
         class Detail {
 
             constructor() {
+                this.id;
+
                 this.initAll().then(() => {
                     this.setNum();      // 设置购买数量
                     this.changeImg();   // 小图片选中样式
                     this.changeLike();  // 改变枪模(like)的选中样式
+                    this.addCart();     // 添加购物车
                 });
             }
 
@@ -53,7 +65,7 @@ require(['./config'], (config) => {
                 // 减少
                 $lessNode.on('click', (e) => {
                     var num = parseInt($numNode.val()); // 当前数量
-                    if(--num < 1){
+                    if (--num < 1) {
                         num = 1;
                     }
                     $numNode.val(num);
@@ -62,7 +74,7 @@ require(['./config'], (config) => {
 
             /** 改变缩略图的选中样式
              ********************************/
-            changeImg(){
+            changeImg() {
                 $("#small").on('click', () => {
                     console.log(this);
                 });
@@ -70,10 +82,81 @@ require(['./config'], (config) => {
 
             /** 改变枪模（like）的选中样式
              **************************************/
-            changeLike(){
-                $("#like").on('click', 'a', function() {
+            changeLike() {
+                $("#like").on('click', 'a', function () {
                     $(this).siblings().removeClass("ac");
                     $(this).addClass("ac");
+                });
+            }
+
+            /** 添加购物车
+             *********************************/
+            addCart() {
+                $("#addCart").on('click', () => {
+                    var list = JSON.parse(localStorage.getItem("cart"));
+                    var id = window.location.search.slice(4);
+
+                    // 添加localStorage数据
+                    if (list) {
+                        // 检验数据存在否(判断id是否存在)
+                        var isExist = list.some((item, index) => {
+                            return item.id === id
+                        });
+
+                        if (isExist) {// 数据已存在， 更新数量
+                            for (let i = 0; i < list.length; i++) {
+                                if (id === list[i].id) {
+                                    var num = $("#num").val(); // 选购数量
+                                    list[i].num = parseInt(list[i].num) + parseInt(num);
+                                    list[i].total += parseFloat(list[i].num,2) + parseFloat(list[i].price,2);
+                                    list[i].total = parseFloat(list[i].total,2);
+                                    break;
+                                }
+                            }
+                            localStorage.setItem("cart", JSON.stringify(list));
+
+                        } else {   // 数据未存在， 添加新数据
+                            var shop = {
+                                id: id,
+                                img: $("#small img").eq(0).attr("src"),
+                                name: $(".shopName").html(),
+                                desc: $(".desc").html(),
+                                price: $(".salePrice").html(),
+                                originPrice: $(".originPrice").html(),
+                                gift: $(".gift").html(),
+                                saleNum: $(".saleNum").html(),
+                                appraiseNum: $(".appraiseNum").html(),
+                                score: $(".score").text(),
+                                like: $("#like").find("a[class='ac']").html(),
+                                num: $("#num").val(),
+                                total:  parseFloat(parseFloat( $("#num").val())*parseFloat($(".salePrice").html())).toFixed(2)
+                                
+                            }
+                            list.push(shop);
+                            localStorage.setItem("cart", JSON.stringify(list));
+                        }
+
+                    } else {   // 添加第一条数据
+                        var shop = {
+                            id: id,
+                            img: $("#small img").eq(0).attr("src"),
+                            name: $(".shopName").html(),
+                            desc: $(".desc").html(),
+                            price: $(".salePrice").html(),
+                            originPrice: $(".originPrice").html(),
+                            gift: $(".gift").html(),
+                            saleNum: $(".saleNum").html(),
+                            appraiseNum: $(".appraiseNum").html(),
+                            score: $(".score").text(),
+                            like: $("#like").find("a[class='ac']").html(),
+                            num: $("#num").val(),
+                            total:  parseFloat(parseFloat( $("#num").val())*parseFloat($(".salePrice").html())).toFixed(2);
+                        }
+                        list = [shop];
+                        localStorage.setItem("cart", JSON.stringify(list));
+                    }
+
+
                 });
             }
 
